8b8166150deeee5ec1f90463cc1c3e7c939e62b6
tumuyan <tumuyan@gmail.com>
2023-05-07 22:08:20 +0800

    定期更新词库

---
+++ basic/rime.lua
@@ -0,0 +1,7 @@
+local M= require("melt")
+get_date = M.getdate
+jpcharset_filter = M.jpcharsetfilter
+long_word_filter = M.longwordfilter
+autocap_filter =M.autocapfilter
+oo_processor = M.ooprocessor
+oo_filter =M.oofilter
