70fd484a064cf15a6b4d4b8ec6c6b8e2697343c3
dao_mingze <dao_mingze@163.com>
2023-08-29 00:31:08 +0800

    fix

--- mrc_releases.sh
+++ mrc_releases.sh
@@ -1,7 +1,7 @@
 mkdir release
 cp -r basic release/rime/
 cp -r schema/. release/rime/
-cp -r ex_dict/. release/rime/
+cp -r dict/. release/rime/
 cp -r custom/. release/rime/
 7zz a release.zip release
 rm -r release
