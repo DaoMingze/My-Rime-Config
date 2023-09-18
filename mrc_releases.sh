b4fd72de111167183c6a48a365ce0f42f8187f13
dao_mingze <dao_mingze@163.com>
2023-09-18 13:33:20 +0800

    release add version num

--- mrc_releases.sh
+++ mrc_releases.sh
@@ -1,7 +1,10 @@
+cur_date=$(date +%Y%m%d)
+name="mrc0.2.1."$cur_date
+echo $name
 mkdir release
 cp -r basic release/rime/
 cp -r schema/. release/rime/
 cp -r dict/. release/rime/
 cp -r custom/. release/rime/
-7zz a release.zip release
+7zz a $name.zip release
 rm -r release
