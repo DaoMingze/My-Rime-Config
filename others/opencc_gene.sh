33cc50e362f36d1d019d10ac7de610304df19845
DaoMingze <34794409+DaoMingze@users.noreply.github.com>
2024-04-21 09:15:48 +0800

    Delete others directory

--- others/opencc_gene.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ./others/opencc
-cp -u ./others/repo/OpenCC/data/config/* ./others/opencc
-cp -u ./others/repo/OpenCC/data/dictionary/* ./others/opencc
-cp -au ./others/opencc/ ./basic
-rm -rf ./others/opencc
