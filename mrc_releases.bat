ec6f4a7ec9c658561101d0aebeda0fffebad2fa1
daomingze <dao_mingze@163.com>
2024-11-25 10:10:00 +0800

    mod mrc_release script

--- mrc_releases.bat
+++ mrc_releases.bat
@@ -3,6 +3,7 @@
 CD /d %~dp0
 :CHOICE
 ECHO.
+set /p filename="版本号: "
 ECHO 1  仅生成一般文件
 ECHO 2  【Android】同文输入法
 ECHO 3  【Windows】小狼毫输入法
@@ -20,7 +21,7 @@ xcopy .\basic .\release\rime\ /E
 xcopy .\schema .\release\rime\ /E
 xcopy .\ex_dict .\release\rime\ /E
 xcopy .\custom .\release\rime\ /E
-7z a release.zip .\release
+7z a "MRC-release-%filename%.7z" .\release
 goto end
 
 :TWO
@@ -32,5 +33,6 @@ xcopy .\theme\weasel.custom.yaml .\release\rime\ /E
 goto  ONE
 
 :end
+rd /s .\release
 echo 完成,请按任意键退出
 pause >nul
